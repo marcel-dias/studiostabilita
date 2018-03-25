15bda49dfdae8adcd85d2245460444cb3b90e6f8
Marcel Dias <marceldiass@gmail.com>
2018-03-25 00:39:45 -0300

    Adicionar descrição nas especialidades

--- assets/js/util.js
+++ assets/js/util.js
@@ -7,7 +7,7 @@
 	$.fn.navList = function() {
 
 		var	$this = $(this);
-			$a = $this.find('a'),
+			$a = $this.find('a');
 			b = [];
 
 		$a.each(function() {
@@ -20,8 +20,8 @@
 			b.push(
 				'<a ' +
 					'class="link depth-' + indent + '"' +
-					( (typeof target !== 'undefined' && target != '') ? ' target="' + target + '"' : '') +
-					( (typeof href !== 'undefined' && href != '') ? ' href="' + href + '"' : '') +
+					( (typeof target !== 'undefined' && target !== '') ? ' target="' + target + '"' : '') +
+					( (typeof href !== 'undefined' && href !== '') ? ' href="' + href + '"' : '') +
 				'>' +
 					'<span class="indent-' + indent + '"></span>' +
 					$this.text() +
@@ -42,7 +42,7 @@
 	$.fn.panel = function(userConfig) {
 
 		// No elements?
-			if (this.length == 0)
+			if (this.length === 0)
 				return $this;
 
 		// Multiple elements?
@@ -95,7 +95,7 @@
 			}, userConfig);
 
 			// Expand "target" if it's not a jQuery object already.
-				if (typeof config.target != 'jQuery')
+				if (typeof config.target !== "jQuery")
 					config.target = $(config.target);
 
 		// Panel.
@@ -153,7 +153,7 @@
 								href = $a.attr('href'),
 								target = $a.attr('target');
 
-							if (!href || href == '#' || href == '' || href == '#' + id)
+							if (!href || href === '#' || href === '' || href === '#' + id)
 								return;
 
 							// Cancel original event.
@@ -182,13 +182,11 @@
 
 					$this.touchPosX = event.originalEvent.touches[0].pageX;
 					$this.touchPosY = event.originalEvent.touches[0].pageY;
-
-				})
+				});
 
 				$this.on('touchmove', function(event) {
 
-					if ($this.touchPosX === null
-					||	$this.touchPosY === null)
+					if ($this.touchPosX === null || $this.touchPosY === null)
 						return;
 
 					var	diffX = $this.touchPosX - event.originalEvent.touches[0].pageX,
@@ -239,8 +237,7 @@
 						}
 
 					// Prevent vertical scrolling past the top or bottom.
-						if (($this.scrollTop() < 0 && diffY < 0)
-						|| (ts > (th - 2) && ts < (th + 2) && diffY > 0)) {
+						if (($this.scrollTop() < 0 && diffY < 0) || (ts > (th - 2) && ts < (th + 2) && diffY > 0)) {
 
 							event.preventDefault();
 							event.stopPropagation();
@@ -282,11 +279,9 @@
 				});
 
 		// Window.
-
 			// Event: Hide on ESC.
 				if (config.hideOnEscape)
 					$window.on('keydown', function(event) {
-
 						if (event.keyCode == 27)
 							$this._hide(event);
 
@@ -307,7 +302,7 @@
 				return $(this);
 
 		// No elements?
-			if (this.length == 0)
+			if (this.length === 0)
 				return $this;
 
 		// Multiple elements?
@@ -329,37 +324,26 @@
 
 					var i = $(this);
 
-					if (i.val() == ''
-					||  i.val() == i.attr('placeholder'))
-						i
-							.addClass('polyfill-placeholder')
-							.val(i.attr('placeholder'));
-
+					if (i.val() === '' ||  i.val() === i.attr('placeholder'))
+						i.addClass('polyfill-placeholder').val(i.attr('placeholder'));
 				})
 				.on('blur', function() {
 
 					var i = $(this);
-
 					if (i.attr('name').match(/-polyfill-field$/))
 						return;
 
-					if (i.val() == '')
+					if (i.val() === '')
 						i
 							.addClass('polyfill-placeholder')
 							.val(i.attr('placeholder'));
-
 				})
 				.on('focus', function() {
-
 					var i = $(this);
-
 					if (i.attr('name').match(/-polyfill-field$/))
 						return;
-
 					if (i.val() == i.attr('placeholder'))
-						i
-							.removeClass('polyfill-placeholder')
-							.val('');
+						i.removeClass('polyfill-placeholder').val('');
 
 				});
 
@@ -377,16 +361,16 @@
 									.replace(/type=password/i, 'type=text')
 					);
 
-					if (i.attr('id') != '')
+					if (i.attr('id') !== '')
 						x.attr('id', i.attr('id') + '-polyfill-field');
 
-					if (i.attr('name') != '')
+					if (i.attr('name') !== '')
 						x.attr('name', i.attr('name') + '-polyfill-field');
 
 					x.addClass('polyfill-placeholder')
 						.val(x.attr('placeholder')).insertAfter(i);
 
-					if (i.val() == '')
+					if (i.val() === '')
 						i.hide();
 					else
 						x.hide();
@@ -398,7 +382,7 @@
 
 							var x = i.parent().find('input[name=' + i.attr('name') + '-polyfill-field]');
 
-							if (i.val() == '') {
+							if (i.val() === '') {
 
 								i.hide();
 								x.show();
@@ -478,7 +462,7 @@
 
 									x = i.parent().find('input[name=' + i.attr('name') + '-polyfill-field]');
 
-									if (i.val() == '') {
+									if (i.val() === '') {
 										i.hide();
 										x.show();
 									}
@@ -498,7 +482,7 @@
 								case 'textarea':
 									i.val(i.attr('defaultValue'));
 
-									if (i.val() == '') {
+									if (i.val() === '') {
 										i.addClass('polyfill-placeholder');
 										i.val(i.attr('placeholder'));
 									}
@@ -528,7 +512,7 @@
 		var key = '__prioritize';
 
 		// Expand $elements if it's not already a jQuery object.
-			if (typeof $elements != 'jQuery')
+			if (typeof $elements !== 'jQuery')
 				$elements = $($elements);
 
 		// Step through elements.
@@ -538,7 +522,7 @@
 					$parent = $e.parent();
 
 				// No parent? Bail.
-					if ($parent.length == 0)
+					if ($parent.length === 0)
 						return;
 
 				// Not moved? Move it.
@@ -552,7 +536,7 @@
 							$p = $e.prev();
 
 							// Couldn't find anything? Means this element's already at the top, so bail.
-								if ($p.length == 0)
+								if ($p.length === 0)
 									return;
 
 						// Move element to top of parent.
